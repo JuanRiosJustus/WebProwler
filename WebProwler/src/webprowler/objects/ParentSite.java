6c19518ce7456e032a5ec6fc1c553cc5a4bdfa2e
Justus Brown <JustusBrownCP@Outlook.com>
2017-09-08 18:42:54 -0700

    Delete ParentSite.java

--- WebProwler/src/webprowler/objects/ParentSite.java
+++
@@ -1,9 +0,0 @@
-package webprowler.objects;
-
-public class ParentSite extends Website 
-{
-	public ParentSite(String nonSearchableURL, String searchableURL) {
-		super(nonSearchableURL, searchableURL);
-		// TODO Auto-generated constructor stub
-	}
-}
