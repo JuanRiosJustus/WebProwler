24e806fce0c8d0e0918bcd1fe7af399ece505023
Justus Brown <JustusBrownCP@Outlook.com>
2017-09-12 22:55:25 -0700

    refactor/overhaul

---
+++ WebProwler/Launcher.java
@@ -0,0 +1,11 @@
+
+import javafx.application.Application;
+import webprowler.gui.Window;
+
+public class Launcher 
+{
+	public static void main (String[] args)
+	{
+		Application.launch(Window.class, args);
+	}
+}
