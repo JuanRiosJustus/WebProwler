4a4f459f509aaabd62c073044317e6cd23b10d85
Justus Brown <JustusBrownCP@Outlook.com>
2017-09-21 21:54:13 -0700

    Add files via upload

--- WebProwler/src/webprowler/objects/Childsite.java
+++ WebProwler/src/webprowler/objects/Childsite.java
@@ -3,12 +3,18 @@ package webprowler.objects;
 public class Childsite extends Website 
 {
 	/* INSTANCE VARIABLES */
-	private String title = "";
-	private String description = "";
-	private String keywords = "";
-	private String text = "";
+	private String title;
+	private String description;
+	private String keywords;
+	private String text;
 	private String[] children;
 	private String[] resources;
+	
+	private String entirety;
+	
+	/* TABLE VARIABLES*/
+	private String domain;
+	private int score;
 	
 	public Childsite(String nonSearchableURL, String searchableURL) 
 	{
@@ -22,15 +28,29 @@ public class Childsite extends Website
 	public String getKeywords() { return keywords; }
 	public String getTitle() { return title; }
 	public String getText() { return text; }
+	public String getSite() { return nonSearchableURL; }
+	public String getEntirety() { return entirety; }
+	
+	public String getDomain() { return domain; }
+	public String getURL() { return nonSearchableURL; }
+	public int getScore() { return score; }
 	
 	/* SET THE WEBSTIE ATTRIBUTES */
-	public void setDescripton(String description) { this.description = this.description + description; }
-	public void setKeywords(String keywords) { this.keywords = this.keywords + keywords; }
+	public void setDescripton(String description) { this.description = description; }
+	public void setKeywords(String keywords) { this.keywords = keywords; }
 	public void setResources(String[] resources) { this.resources = resources;}
 	public void setSite(String siteURL) { this.nonSearchableURL = siteURL; }
 	public void setChildren(String[] children) { this.children = children; }
-	public void setTitle(String title) { this.title = this.title + title; }
-	public void setText(String text) { this.text = this.text + text; }
-
+	public void setTitle(String title) { this.title = title; }
+	public void setText(String text) { this.text = text; }
+	
+	public void setScore(int score) { this.score = score; }
+	public void setDomain(String domain) { this.domain = domain; }
+	
+	public void unifyAllText() 
+	{ 
+		entirety = title + description + keywords + text; 
+	}
+	
 	
 }
