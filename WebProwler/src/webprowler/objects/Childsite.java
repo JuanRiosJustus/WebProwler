fb89cda9706f7092129cc28f934143da3ef3cbd2
Justus Brown <JustusBrownCP@Outlook.com>
2017-09-30 16:00:51 -0400

    ref

--- WebProwler/src/webprowler/objects/Childsite.java
+++ WebProwler/src/webprowler/objects/Childsite.java
@@ -1,56 +1,54 @@
 package webprowler.objects;
 
-public class Childsite extends Website 
+public class Childsite
 {
-	/* INSTANCE VARIABLES */
+	/* Child identifiers*/
+	private String domainURL;
+	private String websiteURL;
+	
+	/* Contents */
 	private String title;
 	private String description;
 	private String keywords;
 	private String text;
 	private String[] children;
-	private String[] resources;
 	
 	private String entirety;
 	
 	/* TABLE VARIABLES*/
-	private String domain;
 	private int score;
 	
-	public Childsite(String nonSearchableURL, String searchableURL) 
+	public Childsite(String websiteURL, String domainURL) 
 	{
-		super(nonSearchableURL, nonSearchableURL);
+		this.websiteURL = websiteURL;
+		this.domainURL = domainURL;
 	}
 	
-	/* GET THE WEBSTIE ATTRIBUTES*/
-	public String getDescripton() { return description; }
-	public String[] getResources() { return resources; }
-	public String[] getChildren() { return children; }
-	public String getKeywords() { return keywords; }
+	/* WEBSTIE ACCESSORS ATTRIBUTES*/
 	public String getTitle() { return title; }
-	public String getText() { return text; }
-	public String getSite() { return nonSearchableURL; }
-	public String getEntirety() { return entirety; }
-	
-	public String getDomain() { return domain; }
-	public String getURL() { return nonSearchableURL; }
-	public int getScore() { return score; }
-	
-	/* SET THE WEBSTIE ATTRIBUTES */
-	public void setDescripton(String description) { this.description = description; }
-	public void setKeywords(String keywords) { this.keywords = keywords; }
-	public void setResources(String[] resources) { this.resources = resources;}
-	public void setSite(String siteURL) { this.nonSearchableURL = siteURL; }
-	public void setChildren(String[] children) { this.children = children; }
 	public void setTitle(String title) { this.title = title; }
+	
+	public String getDescripton() { return description; }
+	public void setDescripton(String description) { this.description = description; }
+	
+	public String getKeywords() { return keywords; }
+	public void setKeywords(String keywords) { this.keywords = keywords; }
+	
+	public String getText() { return text; }
 	public void setText(String text) { this.text = text; }
 	
+	public String getDomainURL() { return domainURL; }
+	public void setDomainURL(String domain) { this.domainURL = domain; }
+	
+	public String getWebsiteURL() { return websiteURL; }
+	public void setWebsiteURL(String websiteURL) { this.websiteURL = websiteURL; } 
+	
+	public int getScore() { return score; }
 	public void setScore(int score) { this.score = score; }
-	public void setDomain(String domain) { this.domain = domain; }
 	
-	public void unifyAllText() 
-	{ 
-		entirety = title + description + keywords + text; 
-	}
+	public String[] getChildren() { return children; }
+	public void setChildren(String[] children) { this.children = children; }
 	
-	
+	public String getEntirety() { return entirety; }
+	public void unifyAllText() { entirety = title + description + keywords + text; }
 }
