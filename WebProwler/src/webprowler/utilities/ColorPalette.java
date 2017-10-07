b060b18461d6b31931e628c2a48d8fa4acd52909
Justus Brown <JustusBrownCP@Outlook.com>
2017-10-07 10:08:34 -0400

    finish

---
+++ WebProwler/src/webprowler/utilities/ColorPalette.java
@@ -0,0 +1,17 @@
+package webprowler.utilities;
+
+public class ColorPalette 
+{
+	/// (LIGHTER) (DARKER)
+	private static String comboBox = "#B3C87A";
+	private static String textField = "#B3C87A";
+	private static String button = "#B3C87A";
+	private static String background = "#347A2A";
+	private static String display = "#EBE8BE";
+	
+	public static String getComboboxColor() { return comboBox; }
+	public static String getTextfieldColor() { return textField; }
+	public static String getButtonColor() { return button; }
+	public static String getBackgroundColor() { return background; }
+	public static String getDisplayColor() { return display; }
+}
